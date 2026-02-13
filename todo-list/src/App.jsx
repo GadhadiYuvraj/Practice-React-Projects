ad70a140ac85b04d243f1f3a1b53de6358b43026
Gadhadi Yuvraj <gadhadiyuvraj@gmail.com>
2026-02-14 04:57:07 +0530

    Updated file

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,13 +1,16 @@
 import { useState } from "react";
+import Header from "./components/header";
+import Footer from "./components/Footer";
+import Todos from "./components/Todos";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        
-      </div>
+      <Header title="45" searchBar={true} />
+      <Footer />
+      <Todos />
     </>
   );
 }
